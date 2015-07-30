115103540e79b3a6ea2be050d727ea7bea55a53b
thomasrstorey <storey.thomas@gmail.com>
2015-07-29 20:36:26 -0400

    basic vision layer support implemented

--- README.md
+++ README.md
@@ -2,16 +2,57 @@
 
 library and API wrapper for Layar webservices providing POI data for geo and vision layers
 
-## Getting Started
-Install the module with: `npm install nayar`
+## Usage
+1. Install the module with: `npm install nayar`
+* setup your database with the included db.sql file
+* load your data into the databse however works for you
+* configure nayar
+* call getPOIs to with an object of Layar getPOIs request parameters:
 
 ```javascript
 var nayar = require('nayar');
-nayar.awesome(); // "awesome"
+nayar.setConfig({user: 'root', pass: 'pass', db: 'nayar_db'});
+//...if using express for instance...
+app.get('/layar', function(req, res){
+  nayar.getPOIs(req.query, function(err, resjson){
+    if(err) console.error(error);
+    res.json(resjson);
+  });
+});
 ```
 
 ## Documentation
-_(Coming soon)_
+### nayar Methods
+
+#### setConfig
+```javascript
+nayar.setConfig([config object]);
+```
+
+#### getConfig
+```javascript
+nayar.getConfig();
+//=> {user: 'user', pass: 'pass', db: 'db', port:'8030'}
+```
+
+#### getPOIs
+```javascript
+nayar.getPOIs(req.query, function(err, resjson){
+  console.dir(resjson);
+});
+//=>{ layer: 'geotest',
+//     hotspots:
+//      [ { id: 'geo_test',
+//          imageURL: 'http://trstorey.sysreturn.net/lib/img/bioav.png',
+//          text:
+//           { title: 'nayartest',
+//             description: 'testing nayar',
+//             footnote: 'author: thomasrstorey' },
+//          anchor: { geolocation: { lat: 40.692842, lon: -73.931183 } } } ],
+//     errorCode: 0,
+//     errorString: 'ok' }
+
+```
 
 ## Examples
 _(Coming soon)_
--- test/nayar_test.js
+++ test/nayar_test.js
@@ -1,7 +1,9 @@
 'use strict';
 var nayar = require('../lib/nayar.js');
-var query = {};
-var expectedResponse = {};
+var geo_query = {};
+var geo_response = {};
+var vision_query = {};
+var vision_response = {};
 var defaultConfig = {};
 var customConfig = {};
 /*
@@ -54,13 +56,11 @@ exports['config'] = {
   }
 };
 
-exports['getGeoPOIs'] = {
+exports['getPOIs'] = {
   setUp: function(done) {
     // setup here
-    // http://devAPI.example.com/getPOIs/?countryCode=IN&lon=4.887339 &userId=ed
-    // 48067cda8e1b985dbb8ff3653a2da4fd490a37 &radius=6245&lat=52.377544&layerNa
-    // me=snowy4&accuracy=100
-    query = {
+    // geo test parameters
+    geo_query = {
       lang : "en",
       countryCode : "US",
       lat : 40.692842,
@@ -72,7 +72,7 @@ exports['getGeoPOIs'] = {
       action : "refresh",
       accuracy : 100
     };
-    expectedResponse = {
+    geo_response = {
       "hotspots": [{
         "id": "geo_test",
         "anchor": { "geolocation": { "lat": 40.692842, "lon": -73.931183 } },
@@ -86,6 +86,84 @@ exports['getGeoPOIs'] = {
         "errorString": "ok",
         "errorCode": 0
     };
+    // vision test parameters
+    vision_query = {
+      lang : "en",
+      countryCode : "US",
+      recognizedReferenceImage : 'menu',
+      userId : "ed48067cda8e1b985dbb8ff3653a2da4fd490a37",
+      radius : 250,
+      layerName : "visiontest",
+      version : "6.0",
+      action : "refresh",
+      accuracy : 100
+    };
+    vision_response = {
+      "hotspots": [{
+        "id": "vision_test",
+        "anchor": { "referenceImage": "menu" },
+        "animations" : {
+          "onCreate" : [{
+              "type" : "translate",
+              "length" : 3000,
+              "delay" : 0,
+              "interpolation" : "linear",
+              "interpolationParam" : null,
+              "persist" : 0,
+              "repeat" : 0,
+              "from" : 1.0,
+              "to" : 0.0,
+              "axis" : {"x": -0.10, "y": 0.0, "z": 0.0}
+          }],
+          "onUpdate" : [],
+          "onDelete" : [],
+          "onFocus" : [{
+            "type" : "scale",
+            "length" : 2000,
+            "delay" : 3000,
+            "interpolation" : "bounce",
+            "interpolationParam" : 1.0,
+            "persist" : 0,
+            "repeat" : 1,
+            "from" : 0.2,
+            "to" : 1.0,
+            "axis" : {"x": 1.0, "y": 1.0, "z": 1.0}
+          }],
+          "onClick" : [{
+            "type" : "rotate",
+            "length" : 1000,
+            "delay" : 0,
+            "interpolation" : "linear",
+            "interpolationParam" : null,
+            "persist" : 1,
+            "repeat" : 1,
+            "from" : 0.0,
+            "to" : 360.0,
+            "axis" : {"x": 0.0, "y": 0.0, "z": 1.0}
+          },{
+            "type" : "translate",
+            "length" : 2000,
+            "delay" : 0,
+            "interpolation" : "accelerateDecelerate",
+            "interpolationParam" : null,
+            "persist" : 1,
+            "repeat" : 0,
+            "from" : 0.0,
+            "to" : 1.0,
+            "axis" : {"x": -0.08, "y": 0.08, "z": 0.0}
+          }]
+        },
+        "object" : {
+          "contentType" : "model/vnd.layar.l3d",
+          "url" : "http://maomao.fixedpoint.nl/temp/layar_l3d/music.l3d",
+          "size" : 0.5
+        }
+        }],
+        "layer": "visiontest",
+        "errorString": "ok",
+        "errorCode": 0
+    };
+    // config for database -- depends on environment where test is running
     customConfig = {
       host : '127.0.0.1',
       user : 'root',
@@ -95,13 +173,21 @@ exports['getGeoPOIs'] = {
     };
     done();
   },
-  'no args': function(test) {
+  'geo': function(test) {
     test.expect(1);
     // tests here
     nayar.setConfig(customConfig);
-    nayar.getPOIs(query, function(json){
-      test.deepEqual(json, expectedResponse, 'should get GeoPOIs from the mysql database and format according to response specification');
+    nayar.getPOIs(geo_query, function(json){
+      test.deepEqual(json, geo_response, 'should produce JSON that matches Layar getPOIs response spec');
       test.done();
     });
   },
+  'vision': function(test) {
+    test.expect(1);
+    nayar.setConfig(customConfig);
+    nayar.getPOIs(vision_query, function(json){
+      test.deepEqual(json, vision_response, 'should produce JSON that matches Layar getPOIs response spec')
+      test.done();
+    });
+  }
 };
